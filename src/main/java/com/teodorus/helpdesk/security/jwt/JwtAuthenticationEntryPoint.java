2762c2bef62bdb2979a20e9e6f5e91defc4393b6
eduardoteodorus <eduardo@vaio>
2018-12-20 02:19:01 -0200

    added simple cors filter

--- src/main/java/com/teodorus/helpdesk/security/jwt/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/teodorus/helpdesk/security/jwt/JwtAuthenticationEntryPoint.java
@@ -8,9 +8,9 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
-@Service
+@Component
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
 	@Override
